8011b0311588b469596c9e6555c9021272cdaa51
Daniyar Telengutov <daniyart2005@gmail.com>
2024-08-13 00:48:47 +0600

    feat: solve carbon dating problem

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string") return false;
+  if (typeof sampleActivity == "string") {
+    if (`${+sampleActivity}` != sampleActivity) {
+      return false;
+    }
+    if (+sampleActivity <= 0 || +sampleActivity > 15) {
+      return false;
+    }
+  }
+
+  return Math.ceil(HALF_LIFE_PERIOD * Math.log2(MODERN_ACTIVITY / sampleActivity));
 }
 
 module.exports = {
