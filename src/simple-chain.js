725c4d44712d4ffd90cb3940f9cfda9666a5be09
Daniyar Telengutov <daniyart2005@gmail.com>
2024-08-16 01:42:26 +0600

    feat: solve simple chain problem

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!Number.isInteger(position) || !(1 <= position && position <= this.getLength())) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = "";
+
+    for (let i = 0; i < this.getLength(); i++) {
+      res += `( ${this.chain[i]} )`;
+
+      if (i != this.getLength() - 1) res += "~~";
+    }
+
+    this.chain = [];
+    return res;
   }
 };
 
