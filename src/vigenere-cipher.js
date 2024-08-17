6214b4ad6691b6b3b4cf69e8e153e91f9594d2bc
Daniyar Telengutov <daniyart2005@gmail.com>
2024-08-18 00:31:47 +0600

    feat: solve vigenere cipher problem

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error("Incorrect arguments!");
+
+    let encryptedMessage = "";
+    const modifiedMesage = message.toUpperCase();
+    const modifiedKey = key.toUpperCase();
+
+    for (let i = 0, j = 0; i < modifiedMesage.length; i++) {
+      const charCode = modifiedMesage[i].charCodeAt(0) - 65;
+      if (0 <= charCode && charCode <= 25) {
+        const keyCode = modifiedKey[j % modifiedKey.length].charCodeAt(0);
+        const shift = keyCode - 65;
+
+        encryptedMessage += String.fromCharCode(65 + ((charCode + shift) % 26));
+
+        j++;
+      }
+      else {
+        encryptedMessage += message[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      encryptedMessage = encryptedMessage.split("").reverse().join("");
+    }
+
+    return encryptedMessage;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error("Incorrect arguments!");
+
+    let message = "";
+    const modifiedKey = key.toUpperCase().split("");
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      const charCode = encryptedMessage[i].charCodeAt(0) - 65;
+      if (0 <= charCode && charCode <= 25) {
+        const keyCode = modifiedKey[j % modifiedKey.length].charCodeAt(0);
+        const shift = keyCode - 65;
+
+        if (charCode - shift >= 0) {
+          message += String.fromCharCode(65 + (charCode - shift));
+        }
+        else {
+          message += String.fromCharCode(65 + (charCode - shift + 26));
+        }
+
+        j++;
+      }
+      else {
+        message += encryptedMessage[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      message = message.split("").reverse().join("");
+    }
+
+    return message;
   }
 }
 
